68ee6e8059c0cf3f22bcd098ed62bccadd5ce900
wesley4121 <cips6768@gmail.com>
2021-04-26 00:18:02 +0800

    01

---
+++ HWH_2D_Midterm_0608457/Assets/我的資料/CS/JoyStickMove.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class JoyStickMove : MonoBehaviour
+{
+    protected Joystick joystick;
+    [Range(1, 200)] public float Speed;
+    void Start()
+    {
+        joystick = FindObjectOfType<Joystick>();
+    }
+
+
+    void Update()
+    {
+       var c_rigibody = gameObject.GetComponent<Rigidbody2D>();
+       c_rigibody.velocity = new Vector2(joystick.Horizontal * Speed, c_rigibody.velocity.y);
+    }
+}
